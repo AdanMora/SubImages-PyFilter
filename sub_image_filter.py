1c869b69be2b42feb2fcbeb0845a114e741b3e31
AdanMora <ajmorafallas@gmail.com>
2018-08-27 18:34:31 -0600

    Output format
    Now you can choose what format you want the sub-images, in Pytorch Tensors, Numpay arrays or PIL Images. By default, returns Pytorch tensors.

--- sub_image_filter.py
+++ sub_image_filter.py
@@ -1,9 +1,9 @@
 from math import sqrt, ceil, floor
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
+import torchvision.transforms as transforms
 
-def calc_columns_rows(n):
+def _calc_columns_rows(n):
     """
     Calculates the number of columns and rows in which the image will be sliced.
     Arg: The number of sub-images.
@@ -13,29 +13,27 @@ def calc_columns_rows(n):
     num_rows = int(ceil(n / float(num_columns)))
     return (num_columns, num_rows)
 
-def getSubImages(img, cant, save=False, path=""):
+def getSubImages(img, cant, save=False, path="", output="Pytorch"):
     """
     Slices the image in a specific number of sub-images.
     Args:
-       img (numpy.array):  The image encoded in numpy array.
+       img (PIL.Image):  The image encoded in PIL Image.
        cant (int):  The number of sub-images to obtain.
     Kwargs:
-       save (bool): Saves the sub-images to a specific path.
-       path (string): Set the specific path.
+       save (bool): Save sub-images to a specific path.
+       path (string): Set specific path.
+       output (string): Set output format: 'Numpy', 'PIL' or 'Pytorch' 
     Returns:
-        Numpy array with the sub-images.
+        List with the sub-images, by default in Pytorch Tensor.
     """
-    
-    im_w, im_h, _ = img.shape
 
-    print(im_w, im_h)
-    
-    img = Image.fromarray(np.uint8(img))
-    columns, rows = calc_columns_rows(cant)
-    extras = (columns * rows) - cant
+    if output not in ("Pytorch", "PIL", "Numpy"):
+            raise ValueError('sub_image_sampler must be \'Pytorch\', \'Numpy\' or \'PIL\'') 
+
+    im_w, im_h = img.size
+
+    columns, rows = _calc_columns_rows(cant)
     sub_img_w, sub_img_h = int(floor(im_w / columns)), int(floor(im_h / rows))
-
-    print(sub_img_w, sub_img_h)
 
     sum_images = []
     i = 0
@@ -46,8 +44,14 @@ def getSubImages(img, cant, save=False, path=""):
 
             if save:
                 image.save(path + "sub_img_" + str(i) + ".png")
-            
-            sum_images.append(np.asarray(image))
+
+            if output == "PIL":
+                sum_images.append(image)
+            elif output == "Pytorch":
+                sum_images.append(transforms.ToTensor()(image))
+            else:
+                sum_images.append(np.asarray(image))
+
             i += 1
     
-    return np.array(sum_images)
+    return sum_images
